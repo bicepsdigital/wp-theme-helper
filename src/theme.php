223d36abb363222e1578f5da2ff3c7f66bc66fef
Lysek <lysek.tomas@gmail.com>
2017-12-18 16:25:15 +0100

    wp coding standarts

--- src/theme.php
+++ src/theme.php
@@ -2,26 +2,26 @@
 
 class WP_Theme_Helper {
 
-	protected static $bodyClasses = array();
+	protected static $body_classes = array();
 	protected static $background = null;
 
-	public static function addBodyClass( $class ) {
-		self::$bodyClasses[ $class ] = $class;
+	public static function add_body_class( $class ) {
+		self::$body_classes[ $class ] = $class;
 	}
 
-	public static function getBodyClasses() {
-		return implode( ' ', self::$bodyClasses );
+	public static function get_body_classes() {
+		return implode( ' ', self::$body_classes );
 	}
 
-	public static function setBackground( $background ) {
+	public static function set_background( $background ) {
 		self::$background = $background;
 	}
 
-	public static function getBackground() {
+	public static function get_background() {
 		return self::$background;
 	}
 
-	public static function getBodyBackgroundModifier( $block ) {
+	public static function get_body_background_modifier( $block ) {
 		if ( ! self::$background ) {
 			return '';
 		}
